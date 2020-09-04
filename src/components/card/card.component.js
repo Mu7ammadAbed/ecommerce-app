bf4780c9ac4b804c78623d018b9a6e2ae7c9e751
Mu7ammad Abed <mu7ammadabed@gmail.com>
2020-09-04 18:39:25 +0300

    Create react app and add homepage card component with its styles

---
+++ src/components/card/card.component.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./card.styles.scss";
+
+const Card = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Clothes</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Clothes</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Clothes</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Clothes</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
