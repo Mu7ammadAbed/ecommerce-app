ada3aca7fc0843fa37b9958aa0b24ca749530f0f
Mu7ammad Abed <mu7ammadabed@gmail.com>
2020-09-04 21:37:39 +0300

    Split landing page contents into components

--- src/containers/landingpage/landingPage.component.js
+++ src/containers/landingpage/landingPage.component.js
@@ -1,41 +1,12 @@
 import React from "react";
+
+import Directory from "../../components/directory/directory.component";
 import "./landingPage.styles.scss";
 
 const LandingPage = () => {
   return (
     <div className="homepage">
-      <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Clothes</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Clothes</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Clothes</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Clothes</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Clothes</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-      </div>
+      <Directory />
     </div>
   );
 };
