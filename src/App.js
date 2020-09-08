a5244805195af7bad9d071e4d1cdf13d1e33faee
Mu7ammad Abed <mu7ammadabed@gmail.com>
2020-09-08 18:33:16 +0300

    Create Header component and style it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
+import Header from "./components/header/header.component";
 import LandingPage from "./containers/landingpage/landingPage.component";
 import ShopPage from "./containers/shop/shop.component";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/shop" component={ShopPage} />
