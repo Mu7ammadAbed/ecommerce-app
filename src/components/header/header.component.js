0867a47a72dbe18809acea7c0e708584d6ef6c97
Mu7ammadAbed <mu7ammadabed@gmail.com>
2020-10-01 10:26:23 +0300

    Create Cart-dropdown menu component and style it

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import { auth } from "../../firebase/firebase.utils";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import "./header.styles.scss";
 
 const Header = ({ user }) => {
@@ -31,6 +32,7 @@ const Header = ({ user }) => {
         )}
         <CartIcon />
       </div>
+      <CartDropdown />
     </div>
   );
 };
