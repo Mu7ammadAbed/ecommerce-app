a92ac745e0d5d3645ad382bb1b897700ec80f522
Mu7ammadAbed <mu7ammadabed@gmail.com>
2020-10-12 18:06:02 +0300

    Add Items to cart using redux

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -2,12 +2,18 @@ import { cartActionTypes } from "./cart.types";
 
 const INITIAL_STATE = {
   hidden: true,
+  cartItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case cartActionTypes.CART_TOGGLE_HIDDEN:
       return { ...state, hidden: !state.hidden };
+    case cartActionTypes.ADD_ITEMS:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload],
+      };
     default:
       return state;
   }
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,3 +1,4 @@
 export const cartActionTypes = {
   CART_TOGGLE_HIDDEN: "CART_TOGGLE_HIDDEN",
+  ADD_ITEMS: "ADD_ITEMS",
 };
